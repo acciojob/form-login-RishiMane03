ec17c00db549399f36e9ed98b5850682dab09e46
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2023-09-28 15:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function getFormvalue() {
-    //Write your code here
+function getFormValue() {
+  // Get the values of the first name and last name input fields
+  var firstName = document.forms["form1"]["fname"].value;
+  var lastName = document.forms["form1"]["lname"].value;
 
+  // Check if both fields are filled
+  if (firstName !== "" && lastName !== "") {
+    // Display the first and last name using alert
+    alert(firstName + " " + lastName);
+  } else {
+    // Show an alert if either of the fields is empty
+    //alert("Please fill in both First Name and Last Name fields.");
+  }
+
+  // Prevent the form from submitting and refreshing the page
+  return false;
 }
